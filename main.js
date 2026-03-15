fa1eadd75532117b2fd2ebe5511debf6b9efbb72
schoolTabber <subianvu@gmail.com>
2026-03-15 17:33:31 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 function frameOpen() {
   var assets = {
     "title" : "Clever | Portal",
-    "icon" : "https://www.clever.com/wp-content/uploads/2023/06/cropped-Favicon-512px-32x32.png"
+    "icon" : "https://assets.clever.com/launchpad/a829c174f/favicon.ico?1"
   };
   var icon = document.getElementById("icon");
   var title = document.getElementById("title");
